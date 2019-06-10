f840bc9eae56dc5ba2cb90ccf4409f4337d356f8
Gunar Ortiz <gunarortiz@gmail.com>
2019-06-10 17:47:13 -0400

    ui simpson

--- interfaz.py
+++ interfaz.py
@@ -11,189 +11,11 @@ from matplotlib.figure import Figure
 
 import numpy as np
 
+from mpmath import *
+mp.pretty = True
+
 from scipy.misc import derivative
 
-def mostrar_tabla(table, out, algo='Newton Raphson'):
-    if out != '':
-        out += '\n'
-
-    if algo == 'Biseccion':
-        for (i, row) in enumerate(table):
-            #print('[' + str(i + 1) + ']', row)
-            out += '[' + str(i + 1) + ']'.join(row)+'\n'
-    else:
-        for row in table:
-            out += str(row) + '\n'
-
-    return out
-
-def prepocess_input(inp):
-    in_str = ''
-    res_str = ''
-    inp = inp.replace(' ','')
-    decomp = inp.split('e')
-    print(decomp)
-
-    for v in decomp:
-        if v == '':
-            continue
-        elif v[0] == 'x' and v[1] == 'p':
-            print('entre1')
-            res_str = res_str + 'e' + v
-        else:
-            if v[0] == '^':
-                print('entre2')
-                res_str = res_str + 'exp' + v[1:]
-            else:
-                print('entre3')
-                res_str += v
-        print('res_str:', res_str)
-    for ch in res_str:
-        if ch == '^':
-            in_str += '**'
-        else:
-            in_str += ch
-
-    print('final value:',in_str)
-    return in_str
-
-def solve_math_errors(f, a, eps):
-    sw = False
-    while sw == False:
-        try:
-            f((a-1)+eps)
-            sw = True
-        except ValueError:
-            a += 1
-    return a
-
-def intersecciones(f, a, b, eps):
-    intersect = []
-    projections = []
-    a = solve_math_errors(f, a, eps)
-
-    prev = f((a-1)+eps)
-    for x_i in range(a,b+1):
-
-        act = f(x_i+eps)
-
-        if act*prev <= 0 + eps:
-            #print((prev, act, act*prev))
-            intersect.append((x_i - 1 + eps, x_i + eps))
-            #projections.append((f(x_i - 1 + eps), f(x_i + eps)))
-        prev = act
-
-    xs = np.linspace(-6.5,6.5, 100)
-    ys = []
-    for x_i in xs:
-        ys.append(f(x_i))
-    return intersect, xs, ys, projections
-
-
-
-
-#no se utiliza
-
-def biseccion(a, b, its, f, tol):
-    a_orig, b_orig = a,b
-    sw = False
-
-    im_X_m = a_orig
-    m_prev = nan
-    #k = 0
-    while sw == False:
-        #k += 1
-        print('a, b:',a,b)
-        #a,b = int(a),int(b)
-        table = []
-        for it in range(its):
-
-            X_m = (a + b) / 2.0
-
-            res = X_m
-
-            if res == m_prev:
-                break
-
-            im_a, im_b, im_X_m = f(a), f(b), f(X_m)
-
-            table.append([str(a) + (' ^+' if im_a > 0 else ' ^-'),
-                         str(b) + (' ^+' if im_b > 0 else ' ^-'),
-                         str(X_m) + (' ^+' if im_X_m > 0 else ' ^-')])
-
-            if im_X_m > 0:
-                b = X_m
-            else:
-                a = X_m
-
-            m_prev = X_m
-
-        if sw == True:
-            break
-
-        if abs(round(f(res),2)) <= tol:
-        #if abs(round(f(res), 2)) <= 0.05:
-            sw = True
-        else:
-            b = a_orig
-            a = b_orig
-
-    #print(k)
-    return res, (True if (f(res) == 0) else False), table
-
-def newton_raphson(a, b, its, f, tol):
-    res = a
-
-    sw = False
-
-    while sw == False:
-        table = []
-        x = Symbol('x')
-        for it in range(its):
-            res = res - (f(res) / derivative(f, res, dx=1e-10))
-            #res = res - f.evalf(subs={x:res}) / diff(f,x).evalf(subs={x:res})
-            # res = res - ( f(res) / dx(res) )
-            table.append(res)
-
-        if sw == True:
-            break
-
-        #0.05
-        if abs(round(f(res),2)) <= tol:
-        #if abs(round(f.evalf(subs={x:res}))) <= tol:
-            sw = True
-        else:
-            res = b
-
-    return res, (True if (f(res) == 0) else False), table
-    #return res, (True if (f.evalf(subs={x:res}) == 0) else False), table
-
-def secante(a, b, its, f, eps, tol):
-    x_i0 = a
-    x_i1 = b
-
-    sw = False
-
-    while sw == False:
-        table = []
-        for it in range(its):
-            temp = (x_i0 * f(x_i1) - x_i1 * f(x_i0)) / (f(x_i1) - f(x_i0) + eps)
-            table.append(temp)
-            x_i0 = x_i1
-            x_i1 = temp
-
-        if sw == True:
-            break
-
-        if abs(round(f(x_i1),2)) <= tol:
-            sw = True
-        else:
-            x_i0 = b
-            x_i1 = a
-
-    return x_i1, (True if (f(x_i1) == 0) else False), table
-
-# hasta aqui no se usa
 
 def diferencias(primer_campo, segundo_campo, tercer_campo):
     f1 = primer_campo
@@ -203,7 +25,6 @@ def diferencias(primer_campo, segundo_campo, tercer_campo):
     f2 = eval("[" + f2 + "]")
 
     n = len(f1)
-    # n = int(input("Ingrese el grado del polinomio a evaluar: ")) + 1
 
     matriz = [0.0] * n
 
@@ -216,8 +37,6 @@ def diferencias(primer_campo, segundo_campo, tercer_campo):
     print(vector)
 
     for i in range(n):
-        # x = input("Ingrese el valor de x: ")
-        # y = input("Ingrese el valor de f("+x+"): ")
         vector[i] = float(f1[i])
         matriz[i][0] = float(f2[i])
 
@@ -265,6 +84,43 @@ def diferencias(primer_campo, segundo_campo, tercer_campo):
 
 
 
+def formulaIntro(x, formula):
+    solucion = solve("-y+" + formula, "y")
+    solucion = eval(str(solucion[0]))
+
+    print(solucion)
+    return solucion
+
+
+def simpson(formula, nodos, a, b):
+
+    m=int(nodos/2)
+
+    h=float((b-a)/(2*m))
+
+    solucion = float(h/3*(formulaIntro(a, formula) + formulaIntro(b, formula)))
+
+    par = 0
+    impar = 0
+
+    for i in range(1, (2*m)):
+        xs = a + (i * h)
+        print("X" + str(i) + ":" + str(xs))
+        xs = formulaIntro(xs, formula)
+        if i%2 == 0:
+            par += xs
+        else:
+            impar += xs
+
+
+    solucion+=float(((2*h)/3) * par + ((4*h)/3) * impar )
+    print(solucion)
+
+    return  formula, solucion, ""
+
+
+
+
 class GUI(tk.Tk):
 
     def __init__(self):
@@ -285,7 +141,7 @@ class GUI(tk.Tk):
         #Menu de metodos
         self.default_choice = tk.StringVar()
         self.default_choice.set('Diferencias Divididas')
-        self.menu = tk.OptionMenu(self, self.default_choice, 'Biseccion', 'Newton Raphson', 'Secante', 'Diferencias Divididas')
+        self.menu = tk.OptionMenu(self, self.default_choice, 'Diferencias Divididas', 'Simpson Compuesto')
 
         self.button = tk.Button(self, text='Resolver', cursor='hand1', command=self.process)
 
@@ -366,78 +222,38 @@ class GUI(tk.Tk):
         print(self.default_choice.get())
 
         self.s_tbl = ''
-        eps = 1e-15
-        #eps = 1e-8
-        tol = 0.05
 
-        #eps = float(self.epsilon.get())
-        #tol = float(self.tolerance.get())
-        iters = 100
         for (k,v) in self.labels.items():
             if k == 'graph':
                 v.get_tk_widget().destroy()
             else:
                 v.destroy()
 
-
-        #aca carga la funcion
-
-        # if self.in_str.get() != 'Diferencias Divididas':
-        #     func = prepocess_input(self.in_str.get().lower())
-        #     transformations = (standard_transformations + (implicit_multiplication_application,))
-        #     X = Symbol('x')
-        #     parsed = parse_expr(func, transformations=transformations)
-        #     func = lambdify((X), parsed)
-        #     intersect, xs, ys, projections = intersecciones(func, -50, 50, eps)
-        #     self.out.set(intersect)
-
-        # row number (starting in 2 because of inputs)
-        #self.r = 2
         self.r = 3
         self.grid_rowconfigure(self.r, weight=1)
 
-        #if(self.epsilon.get() != 'n/a' && self.tolerance.get() != 'n/a'):
-        #    a =
-
-
-        #esto no sirve
-        # print(intersect)
-        # print(projections)
-        # if len(intersect) == 0:
-        #     label = tk.Label(self, text='No se encontraron soluciones')
-        #     label.grid(row=self.r, columnspan=2, sticky='nsew')
-        #     label.config(font=("Ubuntu Mono", 30), bg='white')
-        #     self.labels['lbl0'] = label
-        #     self.r += 1
-        #
-        px,py = [],[]
 
         #Calculando
-        prev = None
-
-        #
-        # if self.default_choice.get() == 'Biseccion':
-        #     res, sw, tbl = biseccion(e[0], e[1], iters, func, tol)
-        # if self.default_choice.get() == 'Newton Raphson':
-        #     #res, sw, tbl = newton_raphson(e[0], e[1], iters, parsed, tol)
-        #     res, sw, tbl = newton_raphson(e[0], e[1], iters, func, tol)
-        # if self.default_choice.get() == 'Secante':
-        #     res, sw, tbl = secante(e[0], e[1], iters, func, 1e-16, tol)
-
-        if self.default_choice.get() == 'Diferencias Divididas':
-            res, aprx, tbl = diferencias(self.primer_campo.get(), self.segundo_campo.get(), self.tercer_campo.get())
-
-        prev = res
-        self.s_tbl = mostrar_tabla(tbl,self.s_tbl, self.default_choice.get())
-        #print(res, '->', abs(round(func(res), 2)))
-        # px.append(res)
-        # py.append(0)
 
         tabla = ""
-        for m in range(len(tbl)):
-            tabla += str(tbl[m]) + "\n"
+        if self.default_choice.get() == 'Diferencias Divididas':
+            res, aprx, tbl = diferencias(self.primer_campo.get(), self.segundo_campo.get(), self.tercer_campo.get())
+            for m in range(len(tbl)):
+                tabla += str(tbl[m]) + "\n"
 
-        label = tk.Label(self, text=tabla + '\nFormula'+': '+str(res) + "\nResultado: " + str(aprx), anchor="e")
+            label = tk.Label(self, text=tabla + '\nFormula' + ': ' + str(res) + "\nResultado: " + str(aprx), anchor="e")
+
+        if self.default_choice.get() == 'Simpson Compuesto':
+            res, aprx, tbl = simpson(self.in_str.get(), int(self.tercer_campo.get()), float(self.primer_campo.get()), float(self.segundo_campo.get()))
+            label = tk.Label(self, text="Resultado: " + str(aprx), anchor="e")
+
+
+        # self.s_tbl = mostrar_tabla(tbl,self.s_tbl, self.default_choice.get())
+
+
+
+
+
         # print(abs(round(func(res), 2)))
         label.grid(row=4, columnspan=2, sticky='W')
         label.config(font=("Ubuntu Mono", 20))
@@ -448,20 +264,20 @@ class GUI(tk.Tk):
         self.r += 1
 
 
-
-
         fig = Figure(figsize=(4,4), dpi = 90)
         a = fig.add_subplot(111)
-        #
-        x = np.arange(0., aprx-aprx/2, 1)
+
+        if self.default_choice.get() == 'Diferencias Divididas':
+            a.plot([5], [25], 'ro')
+            x = np.arange(0., aprx + 10, 1)
+        else:
+            x = np.arange(int(self.primer_campo.get()), int(self.segundo_campo.get())+3, step=1)
+
+
         f = eval(str(res))
 
         a.plot(f)
-        a.plot([5], [25], 'ro')
 
-        # a.plot(px,py, 'ro')
-        # a.axhline(y=0, linewidth=0.5, color='k')
-        # a.axvline(x=0, linewidth=0.5,color='k')
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.get_tk_widget().grid(row=self.r+1, columnspan=2 , sticky = 'nsew')
 
@@ -472,15 +288,6 @@ class GUI(tk.Tk):
         self.labels['tool'] = toolbar
 
         self.labels['graph'] = canvas
-
-        #
-        # m_tbl = tk.Button(self, text='Mostrar Tabla', cursor='hand1', command=self.show_tables)
-        # m_tbl.grid(row=self.r+1, columnspan=2, sticky='nsew')
-        # m_tbl.config(font=("Ubuntu Mono", 30), bg='white')
-        #
-        # self.labels['most_tbl'] = m_tbl
-        # self.grid_rowconfigure(self.r+1, weight=1)
-
 
 if __name__ == "__main__":
     MainWindow = GUI()
